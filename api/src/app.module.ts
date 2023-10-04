11882ef796bea8bbe8cf001a1f476868c28b37b3
hechen77 <2038255225@qq.com>
2023-10-05 00:51:41 +0800

    init:API初始化

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ArticleModule } from './article/article.module';
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { ArticleTypeModule } from './article-type/article-type.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [TypeOrmModule.forRoot({
+    type: 'mysql',
+    host: '139.224.46.4',
+    port: 3306,
+    username: 'freeBlog',
+    password: 'ffxEixSyhbMJK6LN',
+    database: 'freeblog',
+    synchronize: true,
+    autoLoadEntities: true
+  }), ArticleModule, ArticleTypeModule],
 })
-export class AppModule {}
+export class AppModule { }
